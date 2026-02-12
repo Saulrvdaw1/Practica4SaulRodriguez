cf8c494673ab18c9c612d3108b1f971d599866fe
Saul Rodriguez Varilla <srodvar347@g.educaand.es>
2026-02-12 12:40:54 +0100

    Proyecto Practica 4 ED, arreglado el error que modifique sin querer

--- src/com/ejemplo/p4/Main.java
+++ src/com/ejemplo/p4/Main.java
@@ -1,10 +1,8 @@
 package com.ejemplo.p4;
 
 public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) {
+        System.out.println("Practica 4 - Git casos reales");
+        System.out.println("Saúl");
+    }
 }
